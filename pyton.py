74f035c6d6e603797fea996aab97a05b43ba839c
Alireza Romina <alirezaromina@gmail.com>
2026-03-26 21:48:31 +1100

    pytho file

---
+++ pyton.py
@@ -0,0 +1,13 @@
+list = [1, 4, 8 , 9]
+
+def sum(list):
+    sum = 0
+    for i in list:
+        sum += i
+    return sum
+print(sum(list))
+
+list_2 = list + [10, 11, 12]
+print(list_2)
+
+print("why not saving the new file that I added to the branch?")
