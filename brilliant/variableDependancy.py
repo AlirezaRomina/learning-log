b2d15ed8681fe76d50acb03aae9703eba12d6f3c
Alireza Romina <alirezaromina@gmail.com>
2026-03-27 15:56:04 +1100

    added files for practice

---
+++ brilliant/variableDependancy.py
@@ -0,0 +1,12 @@
+username = "MiddlingManager"
+password = "Un78tt!@"
+length = 11
+score = length + 1
+
+uppercase = 2
+score += uppercase
+
+numbers = 1
+score += 2 * numbers
+
+print("Score", score)
